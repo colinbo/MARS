93b99f02ea2ef8ee183b98bec30191c3c3e41f02
Colin <colin218@live.com>
2025-10-27 13:28:51 +0000

    update streaming

--- streaming/prep-project-stream.sh
+++ streaming/prep-project-stream.sh
@@ -10,13 +10,6 @@ else
     gcloud storage buckets create gs://$GOOGLE_CLOUD_PROJECT"-bucket" --soft-delete-duration=0
     gcloud services disable dataflow.googleapis.com --force
     gcloud services enable dataflow.googleapis.com
-    gcloud iam service-accounts create marssa
-    sleep 1
-    gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member serviceAccount:marssa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role roles/editor
-    sleep 1
-    gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member serviceAccount:marssa@$GOOGLE_CLOUD_PROJECT.iam.gserviceaccount.com --role roles/dataflow.worker
-    sleep 1
-    gcloud projects add-iam-policy-binding $GOOGLE_CLOUD_PROJECT --member user:$USER_EMAIL --role roles/iam.serviceAccountUser
     bq mk mars
     bq mk --schema timestamp:STRING,ipaddr:STRING,action:STRING,srcacct:STRING,destacct:STRING,amount:NUMERIC,customername:STRING -t mars.activities
     bq mk --schema message:STRING -t mars.raw
