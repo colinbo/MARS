14c05fa9dbc825d2c3d4fa6ed17f74ecae4f9edb
colinbo <69427912+colinbo@users.noreply.github.com>
2025-10-28 10:41:56 +0800

    Update run-local.sh

--- run-local.sh
+++ run-local.sh
@@ -4,7 +4,7 @@ echo $GOOGLE_CLOUD_PROJECT
 
 sudo pip3 install -r requirements.txt
 gcloud storage cp gs://mars-sample/*.csv sample/
-rm -R output
+
 python3 mars-local.py
 gcloud storage cp output/* gs://$GOOGLE_CLOUD_PROJECT"-bucket/local/"
 bq load --replace=true mars.activities gs://$GOOGLE_CLOUD_PROJECT"-bucket/local/*" 
